ca73a2f68c17b5abb13c93b8f8206f441edba5d8
Shreeyash Pandey <shreeyash335@gmail.com>
2025-12-27 19:02:33 +0530

    forth: refactor word_dict to be leaner
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- forth.cpp
+++ forth.cpp
@@ -30,7 +30,7 @@ private:
 public:
   T pop() {
     if (data.size() == 0) {
-      throw "No elements left in the stack";
+      throw std::runtime_error("No elements left in the stack");
     }
     T val = *(data.end() - 1);
     data.pop_back();
@@ -47,30 +47,81 @@ public:
   }
 };
 
-struct Interpreter {
-  Stack<int> data_stack;
+using State = enum {
+  STATE_INTRP,
+  STATE_COMPILE
 };
 
-std::unordered_map<std::string_view, std::function<int (int, int)>> word_dict {
-  {"+", [](int a, int b) -> int { return a + b; }},
-  {"-", [](int a, int b) -> int { return a - b; }},
-  {"*", [](int a, int b) -> int { return a * b; }},
-  {"/", [](int a, int b) -> int { return a / b; }},
+using WordType = enum {
+  PRIMITIVE,
+  COMPOSITE,
+};
+
+struct Interpreter {
+  State state;
+  Stack<int> data_stack;
+  Interpreter(): state {STATE_INTRP} {}
+};
+
+struct Word {
+  std::string name;
+  WordType word_type; 
+  std::function<void (Interpreter &)> func;
+  std::vector<Word> definition;
+
+  Word(std::string _name, WordType _word_type, std::function<void (Interpreter &)> _func, std::vector<Word> _def):
+    name {_name}, word_type {_word_type}, func {_func}, definition {_def} {}
+};
+
+std::pair<int, int> pop_2(Interpreter &intrp) {
+  int v1 = intrp.data_stack.pop();
+  int v2 = intrp.data_stack.pop();
+  return std::pair {v1, v2};
+}
+
+void primitive_add(Interpreter &intrp) {
+  auto vals = pop_2(intrp);
+  intrp.data_stack.push(vals.first + vals.second);
+}
+void primitive_mul(Interpreter &intrp) {
+  auto vals = pop_2(intrp);
+  intrp.data_stack.push(vals.first * vals.second);
+}
+void primitive_sub(Interpreter &intrp) {
+  auto vals = pop_2(intrp);
+  intrp.data_stack.push(vals.first - vals.second);
+}
+void primitive_div(Interpreter &intrp) {
+  auto vals = pop_2(intrp);
+  intrp.data_stack.push(vals.first / vals.second);
+}
+
+std::unordered_map<std::string_view, Word> word_dict {
+  {"+", Word("+", PRIMITIVE, primitive_add, std::vector<Word>{})},
+  {"*", Word("*", PRIMITIVE, primitive_mul, std::vector<Word>{})},
+  {"-", Word("-", PRIMITIVE, primitive_sub, std::vector<Word>{})},
+  {"/", Word("/", PRIMITIVE, primitive_div, std::vector<Word>{})},
+  {"bye", Word("bye", PRIMITIVE, [](Interpreter &) { std::exit(0); }, std::vector<Word>{})},
 };
 
 void process_command(Interpreter &intrp, std::string_view s) {
   if (s == ".s") {
     intrp.data_stack.print(); 
-  } else if (s == "BYE" || s == "bye") {
-    std::exit(0);
-  } else if (s == "+" || s == "-" || s == "*" || s == "/") {
-    int v1 = intrp.data_stack.pop();
-    int v2 = intrp.data_stack.pop();
-    int res = word_dict[s](v1, v2);
-    intrp.data_stack.push(res);
   } else {
-    std::cout << "Unknown word " << s << '\n';
-  }
+    auto res = word_dict.find(s);
+    if (res != word_dict.end()) {
+      Word &word = res->second;
+      if (word.word_type == PRIMITIVE) {
+        try {
+          word.func(intrp);
+        } catch (const std::runtime_error &e) {
+          std::cout << e.what() << '\n';
+        }
+      }
+    } else {
+      std::cout << "Word not found " << s << '\n';
+    }
+  } 
 }
 
 void repl() {
@@ -79,8 +130,6 @@ void repl() {
   while (std::getline(std::cin, line)) {
     auto new_split =
         line | std::views::drop_while(is_space) | std::views::split(' ');
-
-    // std::cout << std::string_view(*new_split.begin()) << '\n';
     for (const auto &i : new_split) {
       std::string_view line_v = std::string_view(i);
       if (to_int(line_v)) {
